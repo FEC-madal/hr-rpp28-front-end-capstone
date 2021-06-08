a6b98c779c2ed42a15faad2cdf8a65747ac18fce
Thomas Ho <toho42@hotmail.com>
2021-06-07 23:36:25 -0400

    refactored routes with standardized config.js file

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
-const token = '';
-const apiUrl = 'https://app-hrsei-api.herokuapp.com/api/fec2/:hr-rpp/';
+const TOKEN = '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';
+const AUTH = {'Authorization' : TOKEN};
+
+module.exports.AUTH = AUTH;
